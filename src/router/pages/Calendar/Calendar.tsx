d659a8d4fb1aa965ebc9a8322f70fa031f23e6b3
VonRiddarn <vonriddarn@gmail.com>
2025-03-25 00:06:33 +0100

    feat: add calendar debugger to calendar page for more consistent testing

--- src/router/pages/Calendar/Calendar.tsx
+++ src/router/pages/Calendar/Calendar.tsx
@@ -1,9 +1,11 @@
+import DateContextDebugger from "../../../components/debug/DateContextDebugger/DateContextDebugger";
 import CalendarHeader from "./components/CalendarHeader/CalendarHeader";
 
 const Calendar = () => {
 	return (
 		<>
 			<CalendarHeader />
+			<DateContextDebugger />
 		</>
 	);
 };
