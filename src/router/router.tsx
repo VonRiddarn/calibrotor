afc6269e91230abdbfe9f83dc28d391e95f1c19e
VonRiddarn <vonriddarn@gmail.com>
2025-03-24 03:34:25 +0100

    docs: add TODO regarding better system archietecture

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,6 +5,11 @@ import About from "./pages/About";
 import Calendar from "./pages/Calendar";
 import Profile from "./pages/Profile";
 
+// TODO: Add a route for day visualizer or something
+// That's the object in charge of displaying a day graph / log etc.
+// Home will automatically just use this visualizer with the last index of the dict.
+// When viewing dates in the calendar though, this date will be used as a pointer.
+
 const router = createBrowserRouter([
 	{
 		path: "/",
